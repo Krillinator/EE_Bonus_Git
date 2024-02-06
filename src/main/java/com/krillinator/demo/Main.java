ff2a09382ef83945fa4ff3b329431beed6a190f8
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2024-02-06 12:02:23 +0100

    Added: New Comment in Main on line 6

--- src/main/java/com/krillinator/demo/Main.java
+++ src/main/java/com/krillinator/demo/Main.java
@@ -3,7 +3,7 @@ package com.krillinator.demo;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("THE APP IS :)");
+        System.out.println("THE APP IS :)"); // TEST
 
     }
 }
